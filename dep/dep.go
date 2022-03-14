79deec2a9b75ad98df903cb0cf1a31f4f149f779
xixi <xixi@xixideMacBook-Pro.local>
2022-03-14 15:33:44 +0800

    [Add]

---
+++ dep/dep.go
@@ -0,0 +1,9 @@
+package dep
+
+import (
+	"github.com/gogf/gf/util/guid"
+)
+
+func GetUUID() string {
+	return guid.S(nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ddzrc/gomod-test
+
+go 1.17
+
+require github.com/gogf/gf v1.16.6
